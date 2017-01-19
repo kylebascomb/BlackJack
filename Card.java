a0bdac62872fec569cc8826ea306e93992ad5b9c
Kyle Bascomb <kylebascomb@gmail.com>
2017-01-19 09:36:03 -0800

    updated stupid thing

--- Card.java
+++ Card.java
@@ -1,4 +1,3 @@
-
 /**
  * Write a description of class Card here.
  * 
@@ -9,11 +8,14 @@ public class Card
 {
     private String suit;
     private int value;
+    private String card;
+    private final String[] CARDS ={"ACE","TWO","THREE","FOUR","FIVE","SIX","SEVEN","EIGHT","NINE","TEN","JACK","QUEEN","KING"};
     /**
      * Constructor for objects of class Card
      */
-    public Card(int suitInt, int valuea)
+    public Card(int suitInt, int valueA)
     {
+        
         if( suitInt == 1)
             suit = "Spades";
         if(suitInt == 2)
@@ -22,8 +24,12 @@ public class Card
             suit = "Hearts";
         if(suitInt == 4)
             suit = "Diamonds";
+        card = CARDS[valueA-1];
         
-        value = valuea;
+        if(valueA >= 11)
+            value = 10;
+        else
+            value = valueA;
     }
 
     public String getSuit()
@@ -31,9 +37,13 @@ public class Card
         return suit;
     }
     
+     public String getCard()
+    {
+        return card;
+    }
+
     public int getValue()
     {
         return value;
     }
-    }
-
+}
