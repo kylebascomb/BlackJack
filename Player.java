64273e9f86945d7d1998a38f5c3d14ae13fdd8b3
Kyle Bascomb <kylebascomb@gmail.com>
2017-01-19 19:22:22 -0800

    Added a ton of shit. Might be some repetition

--- Player.java
+++ Player.java
@@ -32,8 +32,44 @@ public class Player {
         return tempSum;
     }
     
-    	public void addToHand(Card a){
+    public void addToHand(Card a){
 		hand.add(a);
         }
+    
+    public void removeFromHand(int a){
+    	hand.remove(a);
+    }
+    
+    public boolean checkSplit(){
+    	if(hand.size() > 2)
+    		return false;
+    	Card card1 = hand.get(0);
+    	Card card2 = hand.get(1);
+    	if(card1.getCard().equals(card2.getCard()))
+    		return true;
+    	else
+    		return false;
+    }
+    
+    public boolean checkAce(){
+    	for(int i = 0; i < hand.size(); i++){
+    		Card temp = hand.get(i);
+    		if(temp.getCard().equals("ACE"))
+    			return true;
+    	}
+    	return false;
+    }
+    
+    public Card getCard(int a){
+    	return hand.get(a);
+    }
+    
+    public void printDeck(){
+    	System.out.print(playerName + ": ");
+    	for(int i = 0; i < hand.size(); i++){
+    		Card temp = hand.get(i);
+    		System.out.print(temp.getSuit() + " " + temp.getCard() + " || ");
+    	}
+    }
     
 }
