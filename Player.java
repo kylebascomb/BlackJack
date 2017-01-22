c450e7564871eb5881cffa3b8bbb75cb03ccbe89
Kyle Bascomb <kylebascomb@gmail.com>
2017-01-22 13:56:45 -0800

    fixed ace case

--- Player.java
+++ Player.java
@@ -4,19 +4,19 @@ public class Player {
     private String playerName;
     private ArrayList<Card> hand;
     private boolean dealer;
-    
+
     public Player(String name, boolean isDealer) {
-        
+
         playerName = name;
         hand = new ArrayList<Card>();
         dealer = isDealer;
-        
+
     }
-    
+
     public String getPlayerName() {
         return playerName;
     }
-    
+
     public boolean isDealer(){
         if (dealer == true)
             return true;
@@ -25,57 +25,79 @@ public class Player {
 
     public int getSum() {
         int tempSum = 0;
+        int aceCount = countAce();
         for(int i = 0; i < hand.size(); i++) {
             Card temp = hand.get(i);
             tempSum += temp.getValue();
+            if(tempSum > 21 && aceCount > 0){
+                tempSum = tempSum -10;
+                aceCount--;
+            }
         }
         return tempSum;
     }
-    
-    public int getSumAce() { 
-        return getSum()-10;
-    }
-    
-    public void addToHand(Card a){
-		hand.add(a);
-        }
-    
-    public void removeFromHand(int a){
-    	hand.remove(a);
-    }
-    
-    public boolean checkSplit(){
-    	if(hand.size() > 2)
-    		return false;
-    	Card card1 = hand.get(0);
-    	Card card2 = hand.get(1);
-    	if(card1.getCard().equals(card2.getCard()))
-    		return true;
-    	else
-    		return false;
-    }
-    
-    public boolean checkAce(){
-    	for(int i = 0; i < hand.size(); i++){
-    		Card temp = hand.get(i);
-    		if(temp.getCard().equals("ACE"))
-    			return true;
-    	}
-    	return false;
-    }
-    
-    public Card getCard(int a){
-    	return hand.get(a);
-    }
-    
-    public void printDeck(){
-    	System.out.println(playerName + ": ");
-    	for(int i = 0; i < hand.size(); i++){
-    		Card temp = hand.get(i);
-    		System.out.println(temp.getSuit() + " " + temp.getCard());
-    	}
-    	System.out.println();
-    }
-    
-}
 
+    public int getSumAce() {
+        return getSum() - 10;
+    }
+
+    public void addToHand(Card a){
+        hand.add(a);
+    }
+
+    public void removeFromHand(int a){
+        hand.remove(a);
+    }
+
+    public void eraseHand() {
+        for(int i = 0; i < hand.size(); i++) {
+            hand.remove(i);
+            i--;
+        }
+    }
+
+    public boolean checkSplit(){
+        if(hand.size() > 2)
+            return false;
+        Card card1 = hand.get(0);
+        Card card2 = hand.get(1);
+        if(card1.getCard().equals(card2.getCard()))
+            return true;
+        else
+            return false;
+    }
+
+    public int countAce(){
+        int count = 0;
+        for(int i = 0; i < hand.size(); i++){
+            Card temp = hand.get(i);
+            if(temp.getCard().equals("Ace"))
+                count ++;
+        }
+        return count;
+    }
+
+    public boolean checkFace(){
+        for(int i = 0; i < hand.size(); i++){
+            Card temp = hand.get(i);
+            if(temp.getCard().equals("10") || temp.getCard().equals("Jack") || temp.getCard().equals("Queen") || temp.getCard().equals("King") )
+                return true;
+        }
+        return false;
+    }
+
+    public Card getCard(int a){
+        return hand.get(a);
+    }
+
+    public void printDeck(){
+        System.out.print(playerName + ": ");
+        for(int i = 0; i < hand.size(); i++){
+            Card temp = hand.get(i);
+            System.out.println(temp.getSuit() + " " + temp.getCard());
+        }
+        System.out.println(getSum());
+        System.out.println();
+    }
+
+}
