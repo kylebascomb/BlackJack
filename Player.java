895dbdb5626eec25a9b36818816a35c6a7bdab32
Kyle Bascomb <kylebascomb@gmail.com>
2017-01-24 20:04:46 -0800

    Graphics v.00001

--- Player.java
+++ Player.java
@@ -1,101 +1,226 @@
 
+import javax.swing.*;
+import java.awt.*;
+import javax.imageio.ImageIO;
+import java.io.File;
+import java.awt.image.BufferedImage;
 import java.util.*;
+import java.awt.event.*;
 
-public class Player {
-    private String playerName;
-    private ArrayList<Card> hand;
-    private boolean dealer;
-
-    public Player(String name, boolean isDealer) {
-
-        playerName = name;
-        hand = new ArrayList<Card>();
-        dealer = isDealer;
-
+public class BlackJack extends JPanel{
+    private Deck deck;
+    private ArrayList<Player> playerList;
+    private Player dealer;
+    private BufferedImage image;
+    private int xPos;
+    private int yPos;
+    private int count;
+    JButton hit = new JButton("HIT");
+    JButton stay = new JButton("STAY");
+    JButton doubleDown = new JButton("DOUBLE DOWN");
+    JButton split = new JButton("SPLIT");
+    public BlackJack(){
+        deck = new Deck();
+        playerList = new ArrayList<Player>();
+        dealer = new Player("Dealer",true);
+        xPos = 0;
+        yPos = 0;
+        count = 0;
     }
 
-    public String getPlayerName() {
-        return playerName;
+    public void dealCard(Player temp){
+        image = temp.addToHand(deck.deal());
+        xPos = temp.getXPos();
+        yPos = temp.getYPos();
+        repaint();
+        count--;
+    }
+    
+    public void dealCardStay(Player temp){
+        image = temp.addToHand(deck.deal());
+        xPos = temp.getXPos();
+        yPos = temp.getYPos();
+        repaint();
     }
 
-    public boolean isDealer(){
-        if (dealer == true)
-            return true;
-        return false;
+    public void paintComponent (Graphics g){
+        Graphics2D g2 = (Graphics2D)g;
+        g2.drawImage(image,xPos,yPos,null);
     }
 
-    public int getSum() {
-        int tempSum = 0;
-        int aceCount = countAce();
-        for(int i = 0; i < hand.size(); i++) {
-            Card temp = hand.get(i);
-            tempSum += temp.getValue();
-            if(tempSum > 21 && aceCount > 0){
-                tempSum -= 10;
-                aceCount--;
+    public void initializePlayers(int playerCount){
+        for(int i = 0; i < playerCount ; i++){
+            String name = "Player "+(i+1);
+            Player temp = new Player(name, false);
+            playerList.add(temp);
+            if(i == playerCount-1){
+                playerList.add(dealer);
+            }
+        } 
+    }
+
+    public void clearHand(){
+        for(int i = 0; i < playerList.size(); i++)
+        {
+            Player temp = playerList.get(i);
+            temp.eraseHand();
+        }
+    }
+
+    public void initialDeal(){
+        for(int i = 0; i < playerList.size(); i++){
+            Player temp = playerList.get(i);
+            dealCardStay(temp);
+        }
+
+        for(int i = 0; i < playerList.size(); i++){
+            Player temp = playerList.get(i);
+            dealCardStay(temp);
+        }
+    }
+
+    public void gameLogic(){// Condense more
+        Scanner in = new Scanner(System.in);
+        for(int count = 0; count < playerList.size(); count++){
+            Player temp = playerList.get(count);
+            if(temp.isDealer() == true){
+                
+                if(temp.getSum() < 17){
+                    dealCard(temp);
+                }   
+            }
+            else{   
+                if(temp.getSum() > 21){
+                    temp.printDeck();
+                } else if (temp.getSum() == 21 && temp.countAce() == 1 && temp.checkFace() == true) {
+                    temp.printDeck();
+                }
+                else {
+
+                    if(temp.getSum() < 21 && temp.checkSplit() == true){ 
+                        temp.printDeck();
+                        System.out.println("Enter 1 for hit");
+                        System.out.println("Enter 2 for stay");
+                        System.out.println("Enter 3 for split");
+
+                        int selection = in.nextInt();
+
+                        if(selection == 1){
+                            temp.addToHand(deck.deal());
+                            count--;
+                        }
+                        if(selection == 2){
+                            //nothing
+                        }
+                        if(selection == 3){
+                            Player split = new Player(temp.getPlayerName(),false);
+                            Card splitCard = temp.getCard(1);
+                            temp.removeFromHand(1);
+                            split.addToHand(splitCard);
+                            temp.addToHand(deck.deal());
+                            temp.printDeck();
+                            split.addToHand(deck.deal());
+                            playerList.add(count+1,split);
+                        }
+                    }
+
+                    // else only hit or stay
+                    else {
+                        temp.printDeck();  
+                        if(temp.getSum() < 21){ 
+                            add(hit);
+                            add(stay);
+                            add(doubleDown);
+                            class HitButtonListener implements ActionListener {
+                                public void actionPerformed(ActionEvent event) {
+                                    //things to be done when click on Hit Button (Any Methods)
+                                    dealCard(temp);
+                                }
+                            }
+
+                            class StayButtonListener implements ActionListener {
+                                public void actionPerformed(ActionEvent event) {
+
+                                }
+                            }
+
+                            class DoubleDownButtonListener implements ActionListener {
+                                public void actionPerformed(ActionEvent event) {
+
+                                }
+                            }
+                            ActionListener hitListener = new HitButtonListener();
+                            hit.addActionListener(hitListener);
+                            ActionListener stayListener = new StayButtonListener();
+                            stay.addActionListener(stayListener);
+                            ActionListener doubleDownListener = new DoubleDownButtonListener();
+                            stay.addActionListener(doubleDownListener);
+                        }
+
+                    }
+                }
             }
         }
-        return tempSum;
     }
 
-    public void addToHand(Card a){
-        hand.add(a);
+    public int getDealerSum(){
+        int dealerTotal = 0;
+        for(int i = 0; i < playerList.size(); i++) {
+            Player temp = playerList.get(i);
+            if(temp.isDealer() == true) {
+                dealerTotal = temp.getSum();
+            } 
+        }
+        return dealerTotal;
     }
 
-    public void removeFromHand(int a){
-        hand.remove(a);
-    }
+    public void scoring(){
+        for(int i = 0; i < playerList.size() - 1; i++) {
+            Player temp = playerList.get(i);
 
-    public void eraseHand() {
-        for(int i = 0; i < hand.size(); i++) {
-            hand.remove(i);
-            i--;
+            if(temp.getSum() == 21 && temp.countAce() == 1 && temp.checkFace() == true) {
+                System.out.println(temp.getPlayerName() + ": BlackJack!");
+            } /*else if(temp.getSum() == 21 && temp.checkAce() == true && temp.checkFace() == true) {
+            //TODO If Player and Dealer get BlackJack PUSH
+            }*/ else if(temp.getSum() > 21) {
+                System.out.println(temp.getPlayerName() + ": You Bust!");
+            } else if(temp.getSum() == getDealerSum()) {
+                System.out.println(temp.getPlayerName() + ": Push!");
+            } else if (temp.getSum() > getDealerSum() || getDealerSum() > 21) {
+                System.out.println(temp.getPlayerName() + ": You Win! ");
+            } else {
+                System.out.println(temp.getPlayerName() + ": You Lose! ");
+            }
+
         }
     }
 
-    public boolean checkSplit(){
-        if(hand.size() > 2)
-            return false;
-        Card card1 = hand.get(0);
-        Card card2 = hand.get(1);
-        if(card1.getCard().equals(card2.getCard()))
-            return true;
-        else
-            return false;
-    }
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setSize(1600, 1000);
+        frame.setTitle("BlackJack");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        BlackJack game = new BlackJack();
 
-    public int countAce(){
-        int count = 0;
-        for(int i = 0; i < hand.size(); i++){
-            Card temp = hand.get(i);
-            if(temp.getCard().equals("ACE"))
-                count ++;
+
+        frame.add(game);
+        frame.setVisible(true); 
+        game.initializePlayers(1);
+        boolean cont = true;
+
+        while(cont == true){
+            game.initialDeal();
+            game.gameLogic();
+            game.scoring();
+
+            Scanner answer = new Scanner(System.in);
+            System.out.println("\nWould you like to play again? (y/n)");
+            if(answer.nextLine().equalsIgnoreCase("Y")){
+                cont = true;
+                game.clearHand();
+            }
+            else cont = false;
         }
-        return count;
-    }
 
-    public boolean checkFace(){
-        for(int i = 0; i < hand.size(); i++){
-            Card temp = hand.get(i);
-            if(temp.getCard().equals("10") || temp.getCard().equals("Jack") || temp.getCard().equals("Queen") || temp.getCard().equals("King") )
-                return true;
-        }
-        return false;
     }
-
-    public Card getCard(int a){
-        return hand.get(a);
-    }
-
-    public void printDeck(){
-        System.out.print(playerName + ": ");
-        for(int i = 0; i < hand.size(); i++){
-            Card temp = hand.get(i);
-            System.out.println(temp.getSuit() + " " + temp.getCard());
-        }
-        System.out.println(getSum());
-        System.out.println();
-    }
-
 }
-
